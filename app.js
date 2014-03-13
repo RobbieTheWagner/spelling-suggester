fe00e2a29e329ddaa3b1f1bf45d9379bc3198e2e
Robert Wagner <rwwagner@vt.edu>
2014-03-13 19:41:36 -0400

    Added mocha and should.js

--- app.js
+++ app.js
@@ -2,70 +2,81 @@
 var csv = require('csv');
 var fs = require('fs');
 
-/*Variables*/
-var misspelledWords;
-
-/*Parse the csv and return and array of misspelled words*/
-function getMisspelledWords(callback) {
-    csv().from.stream(fs.createReadStream(__dirname + '/misspelled_queries.csv')).to.array(function (data) {
-            callback(data);
-        });
-}
-
-/*Parse the csv and return an array of dictionary words in descending order by frequency*/
-function sortWords(callback) {
-    csv().from.stream(fs.createReadStream(__dirname + '/word_frequency.csv')).to.array(function (data) {
-        data.sort(function (a, b) {
+/*Parse the csvs and return an array of dictionary words in descending order by frequency and an array of misspelled words.*/
+function parseCsvs(callback) {
+    csv().from.stream(fs.createReadStream(__dirname + '/word_frequency.csv')).to.array(function (dictionary) {
+        dictionary.sort(function (a, b) {
             return b[1] - a[1];
         });
-        //return data;
-        callback(data);
+        csv().from.stream(fs.createReadStream(__dirname + '/misspelled_queries.csv')).to.array(function (misspelled) {
+            callback(dictionary, misspelled);
+        });
     });
 }
 
+parseCsvs(writeSuggestions);
 
-getMisspelledWords(function(test){
-   //console.log(test);
-});
+function calculateLevDistance(word1, word2) {
+    //Holds the distance between the first i characters of word1 and the first j characters of word2.
+    var distances = [];
+    var word1Length = word1.length;
+    var word2Length = word2.length;
+    var i;
+    var j;
+    var cost;
+    while(distances.push([]) <= word1Length);
+    for (i = 0; i <= word1Length; i++) {
+        distances[i][0] = i;
+    }
+    for (j = 0; j <= word2Length; j++) {
+        distances[0][j] = j;
+    }
 
-sortWords(function(test){
-    //console.log(test);
-})
+    for (i = 1; i <= word1Length; i++) {
+        for (j = 1; j <= word2Length; j++) {
+            if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                cost = 0
+            }
+            else {
+                cost = 1;
+            }
+            var deletion = distances[i - 1][j] + 1;
+            var insertion = distances[i][j - 1] + 1;
+            var substitution = distances[i - 1][j - 1] + cost;
+            distances[i][j] = Math.min(deletion, insertion, substitution);
 
+            if (i > 1 && j > 1 && word1.charAt(i) == word2.charAt(j - 1) && word1.charAt(i - 1) == word2.charAt(j)) {
+                distances[i][j] = Math.min(distances[i][j], distances[i - 2][j - 2] + cost);
+            }
+        }
+    }
+    return distances[word1Length][word2Length];
+}
 
-//.to.path(__dirname+'/suggestions.txt')
-/*.transform( function(row){
- row.unshift(row.pop());
- return row;
- })
- .on('record', function(row,index){
- console.log('#'+index+' '+JSON.stringify(row));
- })
- .on('end', function(count){
- console.log('Number of lines: '+count);
- })
- .on('error', function(error){
- console.log(error.message);
- });*/
+function writeSuggestions(dictionary, misspelled)
+{
+    var stream = fs.createWriteStream("suggestions.txt");
 
-
-function determineAlternates(word) {
-    var i, results = [];
-    // deletion
-    for (i = 0; i < word.length; i++)
-        results.push(word.slice(0, i) + word.slice(i + 1));
-    // transposition
-    for (i = 0; i < word.length - 1; i++)
-        results.push(word.slice(0, i) + word.slice(i + 1, i + 2) + word.slice(i, i + 1) + word.slice(i + 2));
-    // alteration
-    for (i = 0; i < word.length; i++)
-        speller.letters.forEach(function (l) {
-            results.push(word.slice(0, i) + l + word.slice(i + 1));
-        });
-    // insertion
-    for (i = 0; i <= word.length; i++)
-        speller.letters.forEach(function (l) {
-            results.push(word.slice(0, i) + l + word.slice(i));
-        });
-    return results;
-};
+    var numWords = 0;
+    for (var i = 0; i < misspelled.length; i++) {
+        var misspelledWord = misspelled[i][0];
+        var writeString = "* " + misspelledWord + ": [";
+        var writeExtraBracket = true;
+        for (var j = 0; j < dictionary.length; j++) {
+            var correctWord = dictionary[j][0];
+            if(calculateLevDistance(misspelledWord, correctWord) <= 2)
+            {
+                writeString += "'" + correctWord + "'" + ",";
+                numWords++;
+                writeExtraBracket = false;
+            }
+        }
+        if (!writeExtraBracket) {
+            writeString = writeString.substring(0, writeString.length - 1);
+        }
+        writeString += "]\r\n";
+        stream.write(writeString);
+    }
+    stream.end();
+    console.log(numWords);
+}
